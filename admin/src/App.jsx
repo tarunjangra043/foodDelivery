3f6f373c72dbc9cd8b91663951d6dcbf70279914
Tarun Jangra <tarunjangra043@gmail.com>
2024-11-16 09:42:32 +0530

    some changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://fooddelbackend-njrb.onrender.com/";
+  const url = "https://fooddelbackend-njrb.onrender.com";
   return (
     <div>
       <ToastContainer />
